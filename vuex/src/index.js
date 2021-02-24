c66b9472ab5c3fdf1dc7d9ae375633316da105a9
yangdingyan <yangdingyan@kedacom.com>
2021-02-24 13:45:43 +0800

    自定义学习分支develop创建

--- vuex/src/index.js
+++ vuex/src/index.js
@@ -1,6 +1,11 @@
 import { Store, install } from './store'
 import { mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from './helpers'
 
+/**
+ * @description:  Vuex对象。vuex的打包后入口对象。
+ * @param {*}
+ * @return {*}
+ */
 export default {
   Store,
   install,
